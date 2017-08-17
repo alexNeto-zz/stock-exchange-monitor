085dc1b84e7cd213d1dbbbeb043a1f28285645b4
alexNeto <alex.neto3@hotmail.com>
2017-08-17 17:35:08 +0000

    fix select/option

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -8,74 +11,91 @@ import { Component, OnInit} from '@angular/core';
 })
 
 export class GraphComponent{
+
+  data: any = {};
+  base = [
+   {id: "EUR", name: "Euro"},
+   {id: "USD", name: "US Dollar"},
+   {id: "BRL", name: "Brazil Real"},
+   {id: "GBP", name: "Great Britain Pound"}
+  ];
+  selectedValue = null;
+  private apiUrl = 'http://api.fixer.io/latest?base='+ (this.selectedValue !== null) ? this.selectedValue : 'USD';
+
+  constructor(private http: Http){
+    console.log('ipsum lorem dolor sit amet');
+    this.getContract();
+    this.getData();
+  }
+
+  getData(){
+    return this.http.get(this.apiUrl)
+    .map((res: Response) => res.json())
+  }
+
+  getContract(){
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data
+    })
+  }
+
   // lineChart
-public lineChartData:Array<any> = [
-  {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-  {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-  {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-];
-public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-public lineChartOptions:any = {
-  responsive: true
-};
-public lineChartColors:Array<any> = [
-  { // grey
-    backgroundColor: 'rgba(148,159,177,0.2)',
-    borderColor: 'rgba(148,159,177,1)',
-    pointBackgroundColor: 'rgba(148,159,177,1)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  },
-  { // dark grey
-    backgroundColor: 'rgba(77,83,96,0.2)',
-    borderColor: 'rgba(77,83,96,1)',
-    pointBackgroundColor: 'rgba(77,83,96,1)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgba(77,83,96,1)'
-  },
-  { // grey
-    backgroundColor: 'rgba(148,159,177,0.2)',
-    borderColor: 'rgba(148,159,177,1)',
-    pointBackgroundColor: 'rgba(148,159,177,1)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  }
-];
-public lineChartLegend:boolean = true;
-public lineChartType:string = 'line';
-
-public randomize():void {
-  let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-  for (let i = 0; i < this.lineChartData.length; i++) {
-    _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-    for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-      _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+  public lineChartData:Array<any> = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
+    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+  ];
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions:any = {
+    responsive: true
+  };
+  public lineChartColors:Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
-  }
-  this.lineChartData = _lineChartData;
-}
+  ];
+  public lineChartLegend:boolean = true;
+  public lineChartType:string = 'line';
 
-// events
-public chartClicked(e:any):void {
-  console.log(e);
-}
-
-public chartHovered(e:any):void {
-  console.log(e);
-}
-}
-
-
-/*
-export class GraphComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public randomize():void {
+    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
+    for (let i = 0; i < this.lineChartData.length; i++) {
+      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+      }
+    }
+    this.lineChartData = _lineChartData;
   }
 
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
 }
-*/
