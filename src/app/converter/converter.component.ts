4b7e0e05c18fb89966a79c60e3c805a231d40f64
alexNeto <alex.neto3@hotmail.com>
2017-08-20 09:05:36 +0000

    commiting uncommited commits

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -11,6 +11,7 @@ export class ConverterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { FixioService } from '../fixio.service'
 
 
 @Component({
@@ -11,41 +10,6 @@ import { FixioService } from '../fixio.service'
 
 export class GraphComponent{
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-  data: any = {};
-  base = [
-   {id: "EUR", name: "Euro"},
-   {id: "USD", name: "US Dollar"},
-   {id: "BRL", name: "Brazil Real"},
-   {id: "GBP", name: "Great Britain Pound"}
-  ];
-  selectedValue = null;
-  private apiUrl = 'http://api.fixer.io/latest?base='+ (this.selectedValue !== null) ? this.selectedValue : 'USD';
-
-  constructor(private http: Http){
-    console.log('conectando ' + this.apiUrl);
-    this.getContract();
-    this.getData();
-    console.log("conectado");
-  }
-  getData(){
-    return this.http.get(this.apiUrl)
-    .map((res: Response) => res.json())
-  }
-  getContract(){
-    this.getData().subscribe(data => {
-      console.log(data);
-      this.data = data
-    })
-  }
-=======
-
-
->>>>>>> working
->>>>>>> 674f3ea42bfae1a437cdd78727c78209dc891d45
-  // lineChart
   public lineChartData:Array<any> = [
     {data: [1,2,3,4,5,6,76,7, 1, 3], label: 'this.stock[0]'},
     {data: [1,2,3,4,5,6,76,7, 2, 1000000], label: 'this.stock[1]'},
@@ -84,23 +48,4 @@ export class GraphComponent{
   public lineChartLegend:boolean = true;
   public lineChartType:string = 'line';
 
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
-
-  // events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
-
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
 }
